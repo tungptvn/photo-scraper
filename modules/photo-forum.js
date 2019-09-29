fa836b6efb9db3d3f204ce124aba69a6f2e07e13
TungPT <tung@Tungs-MacBook-Air.local>
2019-09-29 17:29:43 +0700

    fix bug

--- modules/photo-forum.js
+++ modules/photo-forum.js
@@ -5,7 +5,7 @@ async function runForum() {
 
     var tasks = Array.from(Array(getUserConfig().pTo - getUserConfig().pFrom).keys())
         .map(x => getUserConfig().url + getUserConfig().pageTemplate.replace('__pageNum__', x + getUserConfig().pFrom + 1))
-        .filter(urlRq=> getUserConfig().filterRq && new Regex(getUserConfig().filterRq).test(urlRq)) 
+        .filter(urlRq=> getUserConfig().filterRq && new RegExp(getUserConfig().filterRq).test(urlRq)) 
         .map((url, i) => getImgs(url, i))
 
         renderItems(tasks)
@@ -13,7 +13,7 @@ async function runForum() {
 
 
     var urlex = getUserConfig().url.split('/'), baseUrl = urlex[0] + '//' + urlex[2]
-    var text = setArray(reduce((a, c) => a.concat(c), []))
+    var text = setArray( ret.reduce((a, c) => a.concat(c), []))
         .map(x => /proxy/.test(x) ? baseUrl + '/' + x : x)
         .filter(x => ( getUserConfig().filterImgSrc && new RegExp(getUserConfig().filterImgSrc).test(x)))
         .toString()
