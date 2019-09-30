94f2b19235421ec7d30e61b39164f8cfde20372b
TungPT <tung@Tungs-MacBook-Air.local>
2019-09-30 21:28:54 +0700

    chore

--- modules/config.js
+++ modules/config.js
@@ -1,12 +1,12 @@
 const config = {
     timeDelay: 0,
-    url: '',
+    url: '__url__',
     pFrom: 0, pTo: 10,
     pageTemplate: 'page-__pageNum__',
     filterRq: 'http',
     filterImgSrc: 'http' ,
     isFree: true,
-    crosAnywhere: true
+    crosAnywhere: false
     //pageTemplate: '&page=__pageNum__'
 }
 
